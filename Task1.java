a73ab27601c6d047f742745beaf6872dd8a55eaf
C1lowen <120493291+C1lowen@users.noreply.github.com>
2023-07-12 12:07:32 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,26 @@
+package FileTasks;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        File file = new File("D:\\Kotlin_Project\\untitled\\src\\t.txt");
+
+        System.out.println("Текстовый документ готов к использыванию: ");
+        String str = sc.nextLine();
+
+        try{
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(str);
+            writer.close();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
+}
